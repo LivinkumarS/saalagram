54b4421d44bc1edf64bb90735766fb4ab53e21b9
LivinkumarS <001liv100@ggmail.com>
2024-09-13 08:58:07 +0530

    Seventh Commit

--- server/index.js
+++ server/index.js
@@ -42,8 +42,8 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 // Error Handling
